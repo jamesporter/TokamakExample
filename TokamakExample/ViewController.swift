4b1dca60dc84188d0c7edd2bc63de2c537882f97
James Porter <james@amimetic.co.uk>
2019-04-20 19:41:04 +0100

    Initial Commit

---
+++ TokamakExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TokamakExample
+//
+//  Created by James Porter on 20/04/2019.
+//  Copyright © 2019 James Porter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TokamakExampleTests/TokamakExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  TokamakExampleTests.swift
+//  TokamakExampleTests
+//
+//  Created by James Porter on 20/04/2019.
+//  Copyright © 2019 James Porter. All rights reserved.
+//
+
+import XCTest
+@testable import TokamakExample
+
+class TokamakExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
