03ba547fd9a2f8e0c21604ece38e8c4330056bec
James Porter <james@amimetic.co.uk>
2019-04-21 11:56:26 +0100

    basic working Tokamak example (Counter, with some customisation)

---
+++ TokamakExample/Counter.swift
@@ -0,0 +1,31 @@
+import Tokamak
+
+struct Counter: LeafComponent {
+    struct Props: Equatable {
+        let countFrom: Int
+    }
+    
+    static func render(props: Props, hooks: Hooks) -> AnyNode {
+        let count = hooks.state(props.countFrom)
+        let delta = hooks.state(1)
+        
+        return StackView.node(.init(
+            Edges.equal(to: .parent),
+            axis: .vertical,
+            distribution: .fillEqually), [
+                Button.node(Button.Props(
+                    onPress: Handler { count.set { $0 + delta.value } },
+                    text: "Increment"
+                )),
+                Button.node(Button.Props(
+                    onPress: Handler { count.set { $0 - delta.value } },
+                    text: "Decrement"
+                )),
+                Button.node(Button.Props(
+                    onPress: Handler { delta.set { $0 + 1 } },
+                    text: "Change Delta (\(delta.value))"
+                )),
+                Label.node(.init(alignment: .center, text: "\(count.value)"))
+            ])
+    }
+}
--- TokamakExample/ViewController.swift
+++ TokamakExample/ViewController.swift
@@ -1,20 +1,8 @@
-//
-//  ViewController.swift
-//  TokamakExample
-//
-//  Created by James Porter on 20/04/2019.
-//  Copyright © 2019 James Porter. All rights reserved.
-//
+import Tokamak
+import TokamakUIKit
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+final class ViewController: TokamakViewController {
+    override var node: AnyNode {
+        return Counter.node(.init(countFrom: 1))
     }
-
-
 }
-
